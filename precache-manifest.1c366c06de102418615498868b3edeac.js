0b542b37f5ccd34455afce6ee06ac02a9bace5fd
Fernando Donini Ramos <doniniramos@gmail.com>
2020-04-11 21:00:46 -0300

    Updates

---
+++ precache-manifest.1c366c06de102418615498868b3edeac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7b7c8c2691d165b69ad63d43e0d7c5",
+    "url": "/rpg-dice-web/index.html"
+  },
+  {
+    "revision": "278e06c46167715a93e9",
+    "url": "/rpg-dice-web/static/js/2.a5bba0b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpg-dice-web/static/js/2.a5bba0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbd43649f45fb9aa8a8",
+    "url": "/rpg-dice-web/static/js/main.ecfcccc6.chunk.js"
+  },
+  {
+    "revision": "5212d0d1f0c853089ed7",
+    "url": "/rpg-dice-web/static/js/runtime-main.de3efb27.js"
+  }
+]);
