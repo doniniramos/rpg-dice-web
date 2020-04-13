770bb1df2bedf2cc519245d15c163f59d2f94947
Fernando Donini Ramos <doniniramos@gmail.com>
2020-04-13 14:18:41 -0300

    # Firebase credentials

---
+++ src/utils/firebase_utils.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyD_FZ2yaKKYuaThokUAjfTNsZEW-JQC9yQ",
+    authDomain: "rpg-dice-1d23c.firebaseapp.com",
+    databaseURL: "https://rpg-dice-1d23c.firebaseio.com",
+    projectId: "rpg-dice-1d23c",
+    storageBucket: "rpg-dice-1d23c.appspot.com",
+    messagingSenderId: "263177669502",
+    appId: "1:263177669502:web:fe5a229a52ebf393ed412f",
+    measurementId: "G-BJ1QBBX2PJ"
+};
+
+export const firebaseImpl = firebase.initializeApp(config);
+export const firebaseDatabase = firebase.database();
